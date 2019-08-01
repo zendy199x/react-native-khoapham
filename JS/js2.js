9e6138e6dd6cb383a87e36a8ae585bd2ea8d82f1
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-01 20:47:51 +0700

    reduce

--- JS/js2.js
+++ JS/js2.js
@@ -7,12 +7,18 @@
 // })
 // console.log(newNumbers);
 
-const animals = [
-    {name: 'Cat', age: 10},
-    {name: 'Mouse', age: 15},
-    {name: 'Dog', age: 20}
-]
-const newAnimals = animals.filter(function(animal) {
-    if(animals.age <= 10) return false
-        return true
-})
+// const animals = [
+//     {name: 'Cat', age: 10},
+//     {name: 'Mouse', age: 15},
+//     {name: 'Dog', age: 20}
+// ]
+// const newAnimals = animals.filter(function(animal) {
+//     if(animals.age <= 10) return false
+//         return true
+// })
+
+const numbers = [1, 3, 5, 7, 9]
+const tong = numbers.reduce(function(a, b) {
+    return a + b
+}, 1)
+console.log(tong);
