94c49b1101ca7f45e3048c0e93a112f5b9fc9d78
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-01 20:27:49 +0700

    filter

---
+++ JS/js2.js
@@ -0,0 +1,9 @@
+// //Ham fulter
+// const numbers = [1, 3, 5, 7]
+// //Tao ra 1 mang moi chua gia tri lon hon 1
+// const newNumbers = numbers.filter(function(number) {
+//     if(number <= 1) return false
+//     return true
+// })
+// console.log(newNumbers);
+
