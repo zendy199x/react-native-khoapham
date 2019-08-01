43819a18431cd61f8b113615b18d8d66b88ea2ca
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-01 20:56:29 +0700

    eg reduce

--- JS/js2.js
+++ JS/js2.js
@@ -1,4 +1,4 @@
-// //Ham fulter
+// HAM FILTER
 // const numbers = [1, 3, 5, 7]
 // //Tao ra 1 mang moi chua gia tri lon hon 1
 // const newNumbers = numbers.filter(function(number) {
@@ -6,6 +6,8 @@
 //     return true
 // })
 // console.log(newNumbers);
+
+// eg:
 
 // const animals = [
 //     {name: 'Cat', age: 10},
@@ -17,8 +19,26 @@
 //         return true
 // })
 
-const numbers = [1, 3, 5, 7, 9]
-const tong = numbers.reduce(function(a, b) {
+//HAM REDUCE
+
+// const numbers = [1, 3, 5, 7, 9]
+// const tong = numbers.reduce(function(a, b) {
+//     return a + b
+// }, 1)
+// console.log(tong);
+
+// eg:
+
+const animals = [
+    {name: 'Cat', age: 10},
+    {name: 'Mouse', age: 15},
+    {name: 'Dog', age: 20}
+]
+
+const arrAge = animals.map(function(animal) {
+    return animal.age
+})
+const sumAge = arrAge.reduce(function(a, b) {
     return a + b
-}, 1)
-console.log(tong);
+})
+console.log(sumAge);
