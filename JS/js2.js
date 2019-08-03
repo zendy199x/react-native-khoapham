a219a7d05d2ead5559bb5edc67ce536341aa0afe
Nguyễn Văn Đức <50132805+zendy199x@users.noreply.github.com>
2019-08-03 19:59:49 +0700

    update functional

--- JS/js2.js
+++ JS/js2.js
@@ -1,57 +1,69 @@
-// HAM FILTER
-// const numbers = [1, 3, 5, 7]
-// //Tao ra 1 mang moi chua gia tri lon hon 1
-// const newNumbers = numbers.filter(function(number) {
+// Hàm filter
+// const numbers = [1,3,5,7,9]
+// //  Tạo ra 1 mảng mới chứa giá 
+// //  trị lớn hơn 1
+// const newNumbers = numbers.filter(function(number){
 //     if(number <= 1) return false
 //     return true
 // })
-// console.log(newNumbers);
-
-// eg:
-
+// console.log(newNumbers)
 // const animals = [
-//     {name: 'Cat', age: 10},
-//     {name: 'Mouse', age: 15},
-//     {name: 'Dog', age: 20}
+//     {name : "Mèo" , age : 10},
+//     {name : "Chó" , age : 15},
+//     {name : "Thỏ" , age : 20},
 // ]
-// const newAnimals = animals.filter(function(animal) {
-//     if(animals.age <= 10) return false
-//         return true
+// const newAnimals = animals.filter(function(animal){
+//     if(animal.age <= 10) return false
+//     return true
 // })
-
-//HAM REDUCE
-
-// const numbers = [1, 3, 5, 7, 9]
-// const tong = numbers.reduce(function(a, b) {
-//     return a + b
-// }, 1)
-// console.log(tong);
-
-// eg:
-
+// console.log(newAnimals)
+// const numbers = [2,3,5,7,9]
+// const tong = numbers.reduce(function(a , b ){
+//         console.log("A " + a);
+//         console.log("B " + b);
+//         return a + b
+// })
+// console.log(tong)
 // const animals = [
-//     {name: 'Cat', age: 10},
-//     {name: 'Mouse', age: 15},
-//     {name: 'Dog', age: 20}
+//     {name : "Mèo" , age : 10},
+//     {name : "Chó" , age : 15},
+//     {name : "Thỏ" , age : 20},
 // ]
-
-// const arrAge = animals.map(function(animal) {
+// const arrayAge = animals.map(function(animal){
 //     return animal.age
 // })
-// const sumAge = arrAge.reduce(function(a, b) {
+// const sumage = arrayAge.reduce(function(a,b){
 //     return a + b
 // })
-// console.log(sumAge);
+// console.log(sumage)
 
-Array.prototype.mymap = function(fn){
-    let newArray = []
-    for(let i = 0 ; i < this.length ; i++){
-        const value = fn(this[i])
-        newArray.push(value)
-    }
-    return newArray
+// Array.prototype.mymap = function(fn){
+//     let newArray = []
+//     for(let i = 0 ; i < this.length ; i++){
+//         const value = fn(this[i])
+//         newArray.push(value)
+//     }
+//     return newArray
+// }
+// Array.prototype.myfilter = function(fn){
+//     let newArray = []
+//     for(let i = 0 ; i < this.length ; i++){
+//         const value = fn(this[i])
+//         if(value == true) newArray.push(this[i])
+//     }
+//     return newArray
+// }
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// console.log(numbers.myfilter(function(number){
+//     if(number > 5) return true
+//     return false
+// }))
+//1 : Object
+// const person1 = {name : "Nguyen Van A" , age : 20}
+// 2 : functional
+function Person(Name , Age){
+    this.name = Name
+    this.age = Age
 }
-const numbers = [1,2,3,4,5,6,7,8,9,10]
-console.log(numbers.mymap(function(number){
-    return number
-}))
+const teo = new Person("Teo",10)
+console.log(teo.age)
